40da6e5ebed8a6c483878678cf479121b933ff65
hadi-mubasher <72134354+hadi-mubasher@users.noreply.github.com>
2020-10-22 14:49:14 +0300

    TeacherInfoActivity: information can now be retrieved from the firebase realtime database. Rating can be updated when the student rate a tutor (modifications to this feature will be made).

--- app/src/main/java/com/example/ourapp/TeacherInfoActivity.java
+++ app/src/main/java/com/example/ourapp/TeacherInfoActivity.java
@@ -1,8 +1,12 @@
 package com.example.ourapp;
-
+import com.squareup.picasso.Picasso;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
@@ -12,13 +16,25 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class TeacherInfoActivity extends AppCompatActivity {
+
+
+    String id="BN6PPyaVlGf6fJ2V8wCxwmxEaMf2"; //id of the clicked teacher
+    String oldRating="0";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,17 +43,65 @@ public class TeacherInfoActivity extends AppCompatActivity {
 
         setTitle("TutorApp - Tutor Info");
 
-        EditText teacherName = (EditText) findViewById(R.id.teacherNameETMultiLine);
-        EditText teacherAddress = (EditText) findViewById(R.id.teacherAddressETMultiLine);
-        EditText teacherSubject = (EditText) findViewById(R.id.teacherSubjectsETMultiLine);
-        EditText teacherExperience = (EditText) findViewById(R.id.teacherExperienceETMultiLine);
-        EditText teacherEmail = (EditText) findViewById(R.id.teacherEmail11);
-        EditText teacherPhoneNumber = (EditText) findViewById(R.id.teacherPhone11);
-        EditText teacherSalary = (EditText) findViewById(R.id.editTextSalaryNumberSigned);
-        RatingBar ratingBar = (RatingBar) findViewById(R.id.ratingBar);
-        EditText teacherAge = (EditText) findViewById(R.id.teacherAge);
+        final EditText teacherName = (EditText) findViewById(R.id.teacherNameETMultiLine);
+        final EditText teacherAddress = (EditText) findViewById(R.id.teacherAddressETMultiLine);
+        final EditText teacherSubject = (EditText) findViewById(R.id.teacherSubjectsETMultiLine);
+        final EditText teacherExperience = (EditText) findViewById(R.id.teacherExperienceETMultiLine);
+        final EditText teacherEmail = (EditText) findViewById(R.id.teacherEmail11);
+        final EditText teacherPhoneNumber = (EditText) findViewById(R.id.teacherPhone11);
+        final EditText teacherSalary = (EditText) findViewById(R.id.editTextSalaryNumberSigned);
+        final RatingBar ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+        final EditText teacherAge = (EditText) findViewById(R.id.teacherAge);
+        final ImageView image=(ImageView)  findViewById(R.id.image);
+        DatabaseReference reference= FirebaseDatabase.getInstance().getReference("TutorFormInfo");
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    String name=snapshot.child(id).child("name").getValue(String.class);
+                    String address=snapshot.child(id).child("address").getValue(String.class);
+                    String exp=snapshot.child(id).child("experience").getValue(String.class);
+                    String email=snapshot.child(id).child("email").getValue(String.class);
+                    String phone=snapshot.child(id).child("phoneNum").getValue(String.class);
+                    String salary=snapshot.child(id).child("salary").getValue(String.class);
+                    String age=snapshot.child(id).child("age").getValue(String.class);
+                    String imageUrl=snapshot.child(id).child("imageUrl").getValue(String.class);
+                    String subject=snapshot.child(id).child("subject").getValue(String.class);
+
+
+                    teacherName.setText("Name: "+name);
+                    teacherAddress.setText("Address: "+address);
+                    teacherAge.setText("Age: "+age );
+                    teacherSubject.setText("Subject: "+subject);
+                    teacherExperience.setText("Experience: "+exp);
+                    teacherEmail.setText(Html.fromHtml("Email:<font color='purple'>"+email+" </font>"));
+
+
+
+                    teacherPhoneNumber.setText(Html.fromHtml("Phone Number:<font color='purple'>"+phone+"</font>"));
+                    teacherSalary.setText("Salary: "+salary+" $/hour");
+                    Picasso.get().load(imageUrl).into(image);
+
+
+
+                    }
+
+                      @Override
+                      public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                    });
+
+
+
+
+
+
+
+
+
+
         //teacherName.setText("Name: " + getIntent().getStringExtra("Teacher"));
-        teacherName.setText("Name: Talal Dan");
+   /*     teacherName.setText("Name:"+name);
         teacherAddress.setText("Address: Jounieh, Street 55, Lebanon");
         teacherAge.setText("Age: 31");
         teacherSubject.setText("Subject: Math, physics, chemistry");
@@ -49,11 +113,18 @@ public class TeacherInfoActivity extends AppCompatActivity {
 
         teacherPhoneNumber.setText(Html.fromHtml("Phone Number:<font color='purple'> +961-78-888999</font>"));
         teacherSalary.setText("Salary: 50 USD / hr");
-
+*/
         ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
             @Override
             public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
-                Toast.makeText(TeacherInfoActivity.this, "" + ratingBar.getRating(), Toast.LENGTH_SHORT).show();
+              Toast.makeText(TeacherInfoActivity.this, "" + ratingBar.getRating(), Toast.LENGTH_SHORT).show();
+
+              ratingBar.setIsIndicator(true);             //freeze ratingbar (disable it)
+              float newRating=(ratingBar.getRating()+Float.parseFloat(oldRating))/(float) 2;
+              DatabaseReference updateData = FirebaseDatabase.getInstance().getReference("TutorFormInfo").child(id);
+              updateData.child("rating").setValue(String.valueOf(newRating));
+
+
             }
         });
 
